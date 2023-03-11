2c2ef60d26ad6e90aaba6c5e41d87b92a99705b1
Anthony Suárez <neolight1010@gmail.com>
2023-03-11 11:54:16 -0500

    Add non-functional view for adding logs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use cursive::{
+    view::{Nameable, Resizable},
+    views::{Dialog, EditView, LinearLayout, Panel, SelectView, TextView},
+};
+
+enum MenuSelection {
+    AddLog,
+}
+
 fn main() {
-    println!("Hello World!");
+    let mut siv = cursive::default();
+
+    siv.add_layer(Panel::new(menu_view()));
+
+    siv.run();
+}
+
+fn menu_view() -> SelectView<MenuSelection> {
+    let mut menu = SelectView::<MenuSelection>::new();
+    menu.add_item("Add log", MenuSelection::AddLog);
+
+    menu.set_on_submit(|siv, selection| {
+        siv.pop_layer();
+
+        match selection {
+            MenuSelection::AddLog => {
+                siv.add_layer(add_log_view());
+            }
+        }
+    });
+
+    menu
+}
+
+fn add_log_view() -> Dialog {
+    let layout = LinearLayout::new(cursive::direction::Orientation::Vertical)
+        .child(TextView::new("Product name"))
+        .child(EditView::new().fixed_width(20))
+        .child(TextView::new("Price"))
+        .child(EditView::new().fixed_width(20))
+        .child(TextView::empty().with_name("add_log_result"));
+
+    Dialog::around(layout)
+        .button("Submit", |siv| {
+            let mut add_log_result = siv
+                .find_name::<TextView>("add_log_result")
+                .expect("Couldn't find add_log_result");
+
+            add_log_result.set_content("Log added successfully!");
+        })
+        .h_align(cursive::align::HAlign::Center)
 }
