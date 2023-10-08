63231105b3b235ecd6c52b67600bb957e8129c1b
Anthony Suárez <neolight1010@gmail.com>
2023-10-08 09:32:51 -0500

    Use ExpenditureLog for category_total

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
 use std::collections::HashMap;
 
 pub type Price = f64;
+type Product = String;
+type Category = String;
 
 pub struct ExpenditureLog {
     logs: Vec<(String, Price)>,
+    product_categories: HashMap<Product, Category>,
 }
 
 impl ExpenditureLog {
     pub fn new() -> Self {
-        Self { logs: Vec::new() }
+        Self {
+            logs: Vec::new(),
+            product_categories: HashMap::new(),
+        }
     }
 
     pub fn with_log(self, product: &str, price: Price) -> Self {
@@ -17,6 +23,17 @@ impl ExpenditureLog {
                 .logs
                 .into_iter()
                 .chain(vec![(product.to_owned(), price)])
+                .collect(),
+            ..self
+        }
+    }
+
+    pub fn with_product(self, product: &str, category: &str) -> Self {
+        Self {
+            product_categories: self
+                .product_categories
+                .into_iter()
+                .chain(vec![(product.to_owned(), category.to_owned())])
                 .collect(),
             ..self
         }
@@ -71,10 +88,19 @@ impl ExpenditureLogStats {
     }
 
     pub fn category_total(&self, category: &str) -> Price {
-        self.category_totals
-            .get(category)
-            .unwrap_or(&0.0)
-            .to_owned()
+        self.log
+            .logs
+            .iter()
+            .map(|(product, price)| {
+                if let Some(c) = self.log.product_categories.get(product) {
+                    if c == category {
+                        return price.to_owned();
+                    }
+                }
+
+                return 0.0;
+            })
+            .sum()
     }
 
     pub fn category_totals(&self) -> HashMap<String, Price> {
@@ -104,10 +130,10 @@ mod tests {
 
     #[test]
     fn expenditure_log_category_total() {
-        let mut expenditure_log = ExpenditureLogStats::new(ExpenditureLog::new());
-
-        expenditure_log.add_product("prod1", "cat1");
-        expenditure_log.add_log("prod1", 10.0);
+        let log = ExpenditureLog::new()
+            .with_product("prod1", "cat1")
+            .with_log("prod1", 10.0);
+        let expenditure_log = ExpenditureLogStats::new(log);
 
         assert_eq!(expenditure_log.category_total("cat1"), 10.0);
     }
