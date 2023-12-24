49b1dbe6430b5ff5c4f0972e030764b775fd4d11
Anthony Suárez <neolight1010@gmail.com>
2023-12-24 12:32:20 -0500

    Allow year_month input on add_log view

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use cursive::{
     view::{Nameable, Resizable},
-    views::{Dialog, EditView, LinearLayout, ListView, TextView},
+    views::{Canvas, Dialog, EditView, LinearLayout, ListView, TextView},
 };
 use gregorian::{Month, YearMonth};
 
@@ -15,6 +15,14 @@ pub fn add_log_view() -> Dialog {
         .child(EditView::new().with_name("add_log_name").fixed_width(20))
         .child(TextView::new("Price"))
         .child(EditView::new().with_name("add_log_price").fixed_width(20))
+        .child(TextView::new("Year-Month"))
+        .child(
+            LinearLayout::new(cursive::direction::Orientation::Horizontal)
+                .child(EditView::new().with_name("year_input").fixed_width(5))
+                .child(Canvas::new(()).fixed_width(2))
+                .child(EditView::new().with_name("month_input").fixed_width(3)),
+        )
+        .child(Canvas::new(()))
         .child(TextView::empty().with_name("add_log_result"));
 
     Dialog::around(layout)
@@ -24,21 +32,34 @@ pub fn add_log_view() -> Dialog {
                 .expect("Couldn't find add_log_name")
                 .get_content();
 
-            let add_log_price = siv
+            let price_input = siv
                 .find_name::<EditView>("add_log_price")
                 .expect("Couldn't find add_log_price")
                 .get_content()
                 .parse::<Price>();
 
-            let mut add_log_result = siv
+            let year_input = siv
+                .find_name::<EditView>("year_input")
+                .expect("Couldn't find year_input")
+                .get_content()
+                .parse::<i16>();
+
+            let month_input = siv
+                .find_name::<EditView>("month_input")
+                .expect("Couldn't find month_input")
+                .get_content()
+                .parse::<u8>()
+                .map(|month_str| Month::new(month_str));
+
+            let mut result_view = siv
                 .find_name::<TextView>("add_log_result")
                 .expect("Couldn't find add_log_result");
 
             let current_app = get_finance_app(siv);
             let current_log = current_app.finance();
 
-            match add_log_price {
-                Ok(price) => {
+            match (price_input, year_input, month_input) {
+                (Ok(price), Ok(year), Ok(Ok(month))) => {
                     set_finance_app(
                         siv,
                         current_app.with_finance(
@@ -46,15 +67,15 @@ pub fn add_log_view() -> Dialog {
                             current_log.with_log(FinanceLog::new(
                                 &add_log_name,
                                 price,
-                                YearMonth::new(2021, Month::January),
+                                YearMonth::new(year, month),
                             )),
                         ),
                     );
-                    add_log_result.set_content("Log added successfully!");
+                    result_view.set_content("Log added successfully!");
                 }
 
-                Err(_) => {
-                    add_log_result.set_content("Invalid number!");
+                _ => {
+                    result_view.set_content("Invalid input!");
                 }
             }
         })
