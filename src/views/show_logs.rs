8d3a327dcb143ca40bd3cb1d94f8fbb080ddba3f
Anthony Suárez <neolight1010@gmail.com>
2024-02-12 16:26:36 -0500

    Sort products in log views

--- src/views/show_logs.rs
+++ src/views/show_logs.rs
@@ -4,6 +4,7 @@ use cursive::{
     view::Nameable,
     views::{Dialog, LinearLayout, ListView, Panel, SelectView, TextView},
 };
+use im::Vector;
 use neo_budget::stats::GroupedTotals;
 
 type LogCollection = HashMap<String, GroupedTotals>;
@@ -41,8 +42,8 @@ fn reload_logs_list(log_list: &mut ListView, log_collection: Option<GroupedTotal
     log_list.clear();
 
     if let Some(log_collection) = log_collection {
-        for (product, total) in &log_collection.labeled {
-            add_item_to_log_list(log_list, product, *total);
+        for (product, total) in sorted_labeled_logs(&log_collection) {
+            add_item_to_log_list(log_list, &product, total);
         }
 
         if log_collection.unlabeled > 0.0 {
@@ -51,6 +52,54 @@ fn reload_logs_list(log_list: &mut ListView, log_collection: Option<GroupedTotal
     }
 }
 
+fn sorted_labeled_logs(log_collection: &GroupedTotals) -> Vector<(String, f64)> {
+    let mut vectorized = log_collection
+        .labeled
+        .clone()
+        .into_iter()
+        .collect::<Vector<(String, f64)>>();
+
+    vectorized.sort_by(|left, right| match left.0 < right.0 {
+        true => std::cmp::Ordering::Less,
+        false => std::cmp::Ordering::Greater,
+    });
+
+    vectorized
+}
+
 fn add_item_to_log_list(log_list: &mut ListView, label: &str, total: f64) {
     log_list.add_child(label, TextView::new(format!("{total:.2}")));
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use im::vector;
+    use neo_budget::stats::GroupedTotals;
+
+    use super::sorted_labeled_logs;
+
+    #[test]
+    fn test_sorted_labeled_logs() {
+        let mut labeled = HashMap::new();
+        labeled.insert("b".to_owned(), 2.0);
+        labeled.insert("c".to_owned(), 3.0);
+        labeled.insert("a".to_owned(), 1.0);
+
+        let input = GroupedTotals {
+            labeled,
+            unlabeled: 0.0,
+        };
+        let result = sorted_labeled_logs(&input);
+
+        assert_eq!(
+            result,
+            vector![
+                ("a".to_owned(), 1.0),
+                ("b".to_owned(), 2.0),
+                ("c".to_owned(), 3.0)
+            ],
+        );
+    }
 }
