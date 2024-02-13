44c7b913ed0c99e3c14dcca1c73fabc907e00f54
Anthony Suárez <neolight1010@gmail.com>
2024-02-13 13:27:47 -0500

    Present finance load errors in app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use neo_budget::repository::{EnvJSONFinanceRepository, FinanceRepository};
+use cursive::{
+    views::{Dialog, TextView},
+    Cursive,
+};
+use neo_budget::repository::{
+    EnvJSONFinanceRepository, FinanceRepository, FinanceRepositoryLoadError,
+};
 use views::main_menu::main_menu_view;
 
 use crate::siv::FinanceApp;
@@ -8,23 +14,35 @@ mod views;
 
 fn main() -> Result<(), String> {
     let finance_repo = EnvJSONFinanceRepository::from_env()?;
-    let finance = finance_repo.load()?;
-
-    let finance_app = FinanceApp::new(finance_repo, finance);
+    let finance = finance_repo.load();
 
     let mut siv = cursive::default();
-    siv.set_user_data(finance_app);
-    siv.set_global_callback('q', |siv| {
-        let popped_layer = siv.pop_layer();
+    set_back_button(&mut siv);
 
-        if popped_layer.is_none() {
-            siv.quit();
+    match finance {
+        Ok(finance) => {
+            let finance_app = FinanceApp::new(finance_repo, finance);
+            siv.set_user_data(finance_app);
+
+            siv.add_layer(main_menu_view());
         }
-    });
 
-    siv.add_layer(main_menu_view());
+        Err(error) => siv.add_layer(finance_load_error_view(error)),
+    }
 
     siv.run();
 
     Ok(())
+}
+
+fn set_back_button(siv: &mut Cursive) {
+    siv.set_global_callback('q', |siv| {
+        if siv.pop_layer().is_none() {
+            siv.quit();
+        }
+    });
+}
+
+fn finance_load_error_view(error: FinanceRepositoryLoadError) -> Dialog {
+    Dialog::around(TextView::new(String::from(error)))
 }
